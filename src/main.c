1cf54b5d67523edbaa5c37cc37e104469f3f7a2d
SirObby <70753342+SirObby@users.noreply.github.com>
2024-07-17 19:30:40 +0300

    small change :)

--- src/main.c
+++ src/main.c
@@ -13,7 +13,8 @@ void ev_handler(struct mg_connection *c, int ev, void *ev_data) {
   if (ev == MG_EV_HTTP_MSG) {
     struct mg_http_message *hm = (struct mg_http_message *) ev_data;
     if (mg_match(hm->uri, mg_str("/@*"), NULL)) { 
-      mg_http_reply(c, 200, "Content-Type: text/plain\r\n", "uri: %s\n", hm->uri);
+      mg_http_reply(c, 200, "", "{\"result\": \"%.*s\"}\n", (int) hm->uri.len,
+                    hm->uri.buf);
     } else {
     struct mg_http_serve_opts opts = { .root_dir = "./public/" };
     mg_http_serve_dir(c, hm, &opts);
